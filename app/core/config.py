925f8f9fe7e3a8987eb07c26bbec563bcdb5c41a
Maria <marusjakarassmaria789@gmail.com>
2025-09-05 12:47:51 +0300

    update settings

--- app/core/config.py
+++ app/core/config.py
@@ -2,17 +2,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    # Database & Redis
     database_url: str = (
         "postgresql+psycopg://postgres:postgres@localhost:5432/celery_app"
     )
     redis_url: str = "redis://localhost:6379/0"
     api_title: str = "Celery Application"
 
-    # beat intervals
+    # Beat intervals
     users_interval: int = 300
     addresses_interval: int = 600
     cards_interval: int = 600
 
+    # Credit cards provider
+    cards_provider: str = "remote"  # "remote" or "faker"
+    cards_url: str = "https://random-data-api.com/api/v2/credit_cards"
+    cards_timeout: int = 10
+
+    # Pydantic config
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
 
 
